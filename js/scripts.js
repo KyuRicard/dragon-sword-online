7c466ea06dec28f53b3077d070ab16619e2d6f19
KyuRicard <ricardboscholivar@gmail.com>
2015-05-16 01:08:56 +0200

    Added JSP

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function registre() {
-    var pass1 = document.getElementById('pass1');
-    var pass2 = document.getElementById('pass2');
-    if (pass1 != pass2) {
+    var pass1 = document.getElementById('pass1').value;
+    var pass2 = document.getElementById('pass2').value;
+    if (pass1 !== pass2) {
         alert("Les contrasenyes no coincideixen. Torna a escriure-les");
+        return false;
     } else {
-
+        return true;
     }
 }
