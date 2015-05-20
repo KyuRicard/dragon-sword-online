000baeea577c24078dbc85b3cc687bda754f6868
KyuRicard <ricardboscholivar@gmail.com>
2015-05-20 16:00:06 +0200

    Mod

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ function registre() {
     var pass1 = document.getElementById('pass1').value;
     var pass2 = document.getElementById('pass2').value;
     if (pass1 !== pass2) {
-        alert("Les contrasenyes no coincideixen. Torna a escriure-les");
-        return false;
+        alert("Les contrasenyes no coincideixen. Torna a escriure-les");      
+        pass1.value = "";
+        pass2.value = "";
     } else {
-        return true;
+        
     }
 }
