8a7a3c2baa5ec288e36edce7d31ac40a720d9cda
KyuRicard <ricardboscholivar@gmail.com>
2015-06-10 17:48:11 +0200

    Eliminat Log
    :dango: :cat:

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,13 +3,9 @@ httpServer = require('./httpServer');
 
 data = new Date();
 
-fs.writeFile('logs\\' + data.toDateString() + '.txt');
-log = fs.createWriteStream('logs\\' + data.toDateString() + '.txt');
 
-console.log('Iniciant Servidor de Dragon Sword Online');
-log.write('Iniciant Servidor de Dragon Sword Online\n');    
+console.log('Iniciant Servidor de Dragon Sword Online');   
 console.log(data.toGMTString());
-log.write(data.toGMTString() + '\n');
 
 server = gameServer.Server();
 
